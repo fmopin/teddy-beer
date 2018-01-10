5bb535d88da522ec476ef77d27b2553bb2966136
Antoine Langlet <langlet.antoine@gmail.com>
2018-01-10 20:38:56 +0100

    Test Correction Erreur Liste

--- scraping.js
+++ scraping.js
@@ -63,7 +63,7 @@ module.exports.getBars = function(res, match) {
 
           var str = '{"messages":[{"attachment":{"type":"template","payload":{"template_type":"list","top_element_style":"large","elements":['
 
-          var bars = bars.slice(1,2,3,4);
+          var bars = bars.slice(0,4);
           bars.forEach(function(object, index) {
 
             if (index == bars.length - 1) {
