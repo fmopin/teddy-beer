b5fd3a51613592124579f0835384dd112dc25cea
Florian Mopin <contact@florianmopin.com>
2018-01-10 11:38:46 +0100

    format json bars for chatfuel

--- scraping.js
+++ scraping.js
@@ -9,14 +9,26 @@ module.exports.getBars = function(res, match) {
 
   x(html, '.match-bar', [{
       // data: '.bar-left @data-bar',
-      barName: 'div > .match_bar_top > div.match_bar_name > a@title',
-      latitude:'div > .match_bar_top > div.match_bar_name > p > .latitude > span@title',
-      longitude:'div > .match_bar_top > div.match_bar_name > p > .longitude > span@title',
-      street: 'div > div.match_bar_info > div > p > .street-address',
-      zipCode: 'div > div.match_bar_info > div > p > .postal-code',
-      city: 'div > div.match_bar_info > div > p > .locality',
-      phone: 'div > div.match_bar_info > div > .match-bar-info-phone > span',
-      picture: 'div > div.match_bar_detail > div.match_bar_picture > a > img@src'
+      // barName: 'div > .match_bar_top > div.match_bar_name > a@title',
+      // latitude:'div > .match_bar_top > div.match_bar_name > p > .latitude > span@title',
+      // longitude:'div > .match_bar_top > div.match_bar_name > p > .longitude > span@title',
+      // street: 'div > div.match_bar_info > div > p > .street-address',
+      // zipCode: 'div > div.match_bar_info > div > p > .postal-code',
+      // city: 'div > div.match_bar_info > div > p > .locality',
+      // phone: 'div > div.match_bar_info > div > .match-bar-info-phone > span',
+      // picture: 'div > div.match_bar_detail > div.match_bar_picture > a > img@src'
+
+      title: 'div > .match_bar_top > div.match_bar_name > a@title',
+      image_url: 'div > div.match_bar_detail > div.match_bar_picture > a > img@src',
+      subtitles: 'div > div.match_bar_info > div > p > .street-address',
+      buttons:[
+          {
+            type:"web_url",
+            url:"https://google.com",
+            title:"Google"
+          }
+      ]
+
   }])(function(err, bars) {
       if (err) {
           defer.reject(err);
