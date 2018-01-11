40aae0d0f63ddd71251b1843e19e5a780d570cc2
Antoine Langlet <langlet.antoine@gmail.com>
2018-01-11 10:33:57 +0100

    Test Gallery

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var bars = require('./routes/bars');
+var fixtures = require('./routes/fixtures');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/bars', bars);
+app.use('/fixtures', fixtures);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
