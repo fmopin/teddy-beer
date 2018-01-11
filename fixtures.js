407ee58be52c57292ac491f654de65795bbcf78a
Antoine Langlet <langlet.antoine@gmail.com>
2018-01-11 10:41:37 +0100

    Test Gallery

--- fixtures.js
+++ fixtures.js
@@ -1,4 +1,4 @@
-module.exports.getFixtures = function(res, match) {
+module.exports.getFixtures = function(res) {
   var url = 'https://api.football-data.org/v1/competitions/450/fixtures';
 
   http.get(url, function(res){
