5431bd3bd8f911378fa1a74bc2d23d7e21f2553d
Antoine Langlet <langlet.antoine@gmail.com>
2018-01-11 10:52:28 +0100

    Test Gallery

--- scraping.js
+++ scraping.js
@@ -49,7 +49,7 @@ module.exports.getBars = function(res, match) {
 
           // FAKE DATA
 
-          // var str = '{"messages":[{"attachment":{"type":"template","payload":{"template_type":"list","top_element_style":"large","elements":[{"title":"Ceci est un titre avec l\'apostrophe","image_url":"http://rockets.chatfuel.com/img/shirt.png","subtitle":"ceci est un subtitle"},{"title":"Ceci est un titre","image_url":"http://rockets.chatfuel.com/img/shirt.png","subtitle":"ceci est un subtitle"}]}}}]}'
+          var str = '{"messages":[{"attachment":{"type":"template","payload":{"template_type":"generic","image_aspect_ratio":"square","elements":[{"title":"Chatfuel Rockets T-Shirt","image_url":"https://rockets.chatfuel.com/img/shirt.png","subtitle":"Soft white cotton t-shirt with CF Rockets logo","buttons":[{"type":"web_url","url":"https://rockets.chatfuel.com/store/shirt","title":"View Item"}]},{"title":"Chatfuel Rockets Hoodie","image_url":"https://rockets.chatfuel.com/img/hoodie.png","subtitle":"Soft grey cotton hoddie with CF Rockets logo","default_action":{"type":"web_url","url":"https://rockets.chatfuel.com/store","messenger_extensions":true},"buttons":[{"type":"web_url","url":"https://rockets.chatfuel.com/store/hoodie","title":"View Item"}]}]}}}]}'
 
 
 
@@ -61,22 +61,22 @@ module.exports.getBars = function(res, match) {
 
           // REAL DATA WITHTOUT TITLE
 
-          var str = '{"messages":[{"attachment":{"type":"template","payload":{"template_type":"list","top_element_style":"large","elements":['
-
-          var bars = bars.slice(0,4);
-
-          bars.forEach(function(object, index) {
-
-            if (index == bars.length - 1) {
-              str += '{"title":"' + object.titleScraped + '", "image_url":"' + object.imageScraped + '", "subtitle":"' + object.subtitleScraped + '"}';
-            }
-            else {
-              str += '{"title":"' + object.titleScraped + '", "image_url":"' + object.imageScraped + '", "subtitle":"' + object.subtitleScraped + '"},';
-            }
-
-          })
-
-          str += ']}}}]}'
+          // var str = '{"messages":[{"attachment":{"type":"template","payload":{"template_type":"list","top_element_style":"large","elements":['
+          //
+          // var bars = bars.slice(0,4);
+          //
+          // bars.forEach(function(object, index) {
+          //
+          //   if (index == bars.length - 1) {
+          //     str += '{"title":"' + object.titleScraped + '", "image_url":"' + object.imageScraped + '", "subtitle":"' + object.subtitleScraped + '"}';
+          //   }
+          //   else {
+          //     str += '{"title":"' + object.titleScraped + '", "image_url":"' + object.imageScraped + '", "subtitle":"' + object.subtitleScraped + '"},';
+          //   }
+          //
+          // })
+          //
+          // str += ']}}}]}'
 
           // console.log("Obj to str : ");
           var object = JSON.parse(str);
